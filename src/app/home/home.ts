c0cedefbd7b62a07ead26b7d808195136cf2148c
zedatra <drazeta.milos@gmail.com>
2025-07-01 20:38:12 +0200

    Implemented if and for each loops, bun installed

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FilmService } from '../../services/main.service';
+import { JsonPipe, NgFor, NgIf } from '@angular/common';
+import { AxiosError } from 'axios';
+import { FilmModel } from '../../models/film.model';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [JsonPipe, NgIf, NgFor],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  filmovi: FilmModel[] | null = null
+  error: string | null = null
+
+  constructor(){
+    FilmService.getFilmovi()
+      .then(rsp => this.filmovi = rsp.data.results)
+      .catch((e:AxiosError) => this.error = `${e.code}: ${e.message}`)
+  }
 
 }
